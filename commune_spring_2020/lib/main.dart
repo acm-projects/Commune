1aa439231123922dfd3a6c18bec9b7ea5f36cde0
Olimjon <olimjon.nematov@gmail.com>
2020-02-22 09:11:32 -0600

    testing branch commit

--- commune_spring_2020/lib/main.dart
+++ commune_spring_2020/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-
+//test 1 234 i was here
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
