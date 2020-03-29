55b8fa6057d6001eff73599c0b78b8632e70d242
Olimjon <olimjon.nematov@gmail.com>
2020-03-29 00:23:25 -0500

    wrapper fixed

--- commune_spring_2020/lib/main.dart
+++ commune_spring_2020/lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:commune_spring_2020/Models/User.dart';
 import 'package:commune_spring_2020/Pages/householdprofile.dart';
-//import 'package:commune_spring_2020/Pages/login.dart';
-//import 'package:commune_spring_2020/screens/wrapper.dart';
+// import 'package:commune_spring_2020/Pages/login.dart';
+import 'package:commune_spring_2020/screens/wrapper.dart';
 import 'package:commune_spring_2020/screens/auth/AccountAccess.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:commune_spring_2020/Pages/homepage.dart';
-//import 'package:commune_spring_2020/Pages/register_user.dart';
-//import 'package:commune_spring_2020/Pages/welcome.dart';
+// import 'package:commune_spring_2020/Pages/register_user.dart';
+// import 'package:commune_spring_2020/Pages/welcome.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -18,8 +18,8 @@ class MyApp extends StatelessWidget {
     return StreamProvider<User>.value(
       value: AuthService().user,
       child: MaterialApp(
-        ///home: Wrapper(),
-        home: HouseholdProfile(),
+        home: Wrapper(),
+        // home: HouseholdProfile(),
       ),
     );
   }
--- commune_spring_2020/lib/screens/wrapper.dart
+++ commune_spring_2020/lib/screens/wrapper.dart
@@ -9,35 +9,60 @@ import 'package:commune_spring_2020/screens/Home_Files/home.dart';
 class Wrapper extends StatelessWidget {
   String currentHouseholdName;
 
+  /*
+   *          This widget returns either the home screen or the auth screen. If the user is signed in it will 
+   *          return the home widget. otherwise it will return the sign in page.
+   * 
+   * 
+   * 
+   *                           -> if user is signed in      -> Home      ->rest of the app    
+   *                          |                                 ^  
+   *                          |                                 |
+   *          main  ->  wrapper                                  -----------------------                         
+   *                          |                                                         |
+   *                           -> if user is not signed in  ->  Sign In  ->   once singed in/up
+   *                                          
+   *          
+   *          Don't add a wiget bc it will mess up the provider (widget that listens to when ).
+   *          Instead, in the home page, create an if statement to return the proper widget
+   *          
+   *          
+   *          
+  */
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
     
-    //retrun the proper widget
-    if(user==null){
+    if (user == null){
       return Authenticate();
-    }else{
-     return Container(
-       height: MediaQuery.of(context).size.height/1.5,
-       child: FutureBuilder<String>(
-          future: getUserHousehold(user.uid),
-          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-            if (snapshot.hasData) {
-              if(currentHouseholdName=='Null')
-              {
-                return HouseHoldSelectionPage();
-              }
-              else
-              {
-                return Home(uid: user.uid);
-              }
-            } else {
-              return Text('Getting Household Name...');
-            }
-          },
-        )
-     );
+    } else {
+      return Home();
     }
+    //retrun the proper widget
+  //   if(user==null){
+  //     return Authenticate();
+  //   }else{
+  //    return Container(
+  //      height: MediaQuery.of(context).size.height/1.5,
+  //      child: FutureBuilder<String>(
+  //         future: getUserHousehold(user.uid),
+  //         builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+  //           if (snapshot.hasData) {
+  //             if(currentHouseholdName=='Null')
+  //             {
+  //               return HouseHoldSelectionPage();
+  //             }
+  //             else
+  //             {
+  //               return Home(uid: user.uid);
+  //             }
+  //           } else {
+  //             return Text('Getting Household Name...');
+  //           }
+  //         },
+  //       )
+  //    );
+  //   }
   }
 
    Future<String> getUserHousehold(String userID) async {
